b5e24c42bb2cd4648ab6f90dcaaa1cd1f55759eb
Adenaan <adenaan@gmail.com>
2026-01-30 13:32:22 +0200

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ink: "#061127",
+        navy: "#07122a",
+        gold: "#d8b15a",
+        sand: "#f1e7c7",
+      },
+      fontFamily: {
+        display: ['ui-serif','Georgia','Cambria','"Times New Roman"','Times','serif'],
+      },
+    },
+  },
+  plugins: [],
+};
